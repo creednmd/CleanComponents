2afb4f8fc35745f0834ee703e6a17c1fe27a3f93
Andy Warwick <andy.w@creed.co.uk>
2019-04-03 08:26:01 +0100

    Initial project setup

---
+++ CleanComponents/CleanComponents.h
@@ -0,0 +1,19 @@
+//
+//  CleanComponents.h
+//  CleanComponents
+//
+//  Created by Andy Warwick on 03/04/2019.
+//  Copyright © 2019 Andrew Warwick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CleanComponents.
+FOUNDATION_EXPORT double CleanComponentsVersionNumber;
+
+//! Project version string for CleanComponents.
+FOUNDATION_EXPORT const unsigned char CleanComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanComponents/PublicHeader.h>
+
+
